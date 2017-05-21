45e05c9e51a068167b341ffca6d92439089b4773
Hiroki Noda <kubo39@gmail.com>
2017-05-21 23:22:27 +0900

    Support FreeBSD

--- src/ncpu.cr
+++ src/ncpu.cr
@@ -2,7 +2,7 @@ require "./ncpu/*"
 
 module Ncpu
   def self.get
-    {% if flag?(:linux) || flag?(:darwin) %}  # or Solaris
+    {% if flag?(:linux) || flag?(:darwin) || flag?(:freebsd) %}  # or Solaris
        LibC.sysconf(LibC::SC_NPROCESSORS_ONLN)
     {% else %}
       {{ raise "Don't know how to count N CPUs on this machine." }}
--- src/ncpu/libc.cr
+++ src/ncpu/libc.cr
@@ -1,8 +1,9 @@
 lib LibC
   {% if flag?(:linux) %}
     SC_NPROCESSORS_ONLN = 84
-  {% elsif flag?(:darwin) %}
+  {% elsif flag?(:darwin) || flag?(:freebsd) %}
     SC_NPROCESSORS_ONLN = 58
   {% end %}
+
   fun sysconf(name : LibC::Int) : LibC::Long
 end
